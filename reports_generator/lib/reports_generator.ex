26be40345f09f42be4b4fde439ff85b843e3e923
Joao Carvalho <joaokvalho@gmail.com>
2021-08-09 17:24:30 -0300

    Add test Parser

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,6 +1,19 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @available_food [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -9,12 +22,27 @@ defmodule ReportsGenerator do
     end)
   end
 
-  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
-  # defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    # report
+    # |> Map.put("users", users)
+    # |> Map.put("foods", foods)
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  # defp report_list_generator, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
   defp report_list_generator() do
-    Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)
+    foods = Enum.into(@available_food, %{}, fn x -> {x, 0} end)
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
   end
 end
