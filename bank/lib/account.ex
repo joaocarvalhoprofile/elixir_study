1402710549b70d88c327ec789cba357d60c9d348
Joao Carvalho <joaokvalho@gmail.com>
2021-09-13 16:36:59 -0300

    Add account with db/file

--- bank/lib/account.ex
+++ bank/lib/account.ex
@@ -1,22 +1,50 @@
 defmodule Account do
+  @accounts "db/account.txt"
+
   defstruct user: User, balance: 1000
 
   def register(user) do
-    %__MODULE__{user: user}
+    # account =
+    #   [%__MODULE__{user: user}]
+    #   |> :erlang.term_to_binary()
+
+    # File.write(@accounts, account)
+    case find_by_email(user.email) do
+      nil ->
+        account =
+          ([%__MODULE__{user: user}] ++ read_account())
+          |> :erlang.term_to_binary()
+
+        File.write(@accounts, account)
+
+      _ ->
+        {:error, "Existing Account"}
+    end
   end
 
-  def pix(accounts, of, afor, avalue) do
-    of = Enum.find(accounts, fn account -> account.user.email == of.user.email end)
+  def read_account do
+    {:ok, result} = File.read(@accounts)
+    :erlang.binary_to_term(result)
+  end
+
+  def find_by_email(email), do: Enum.find(read_account(), &(&1.user.email == email))
+
+  def pix(of, afor, avalue) do
+    of = find_by_email(of.user.email)
 
     cond do
       valid_balance(of.balance, avalue) ->
         {:error, "Insufficient balance"}
 
       true ->
-        afor = Enum.find(accounts, fn account -> account.user.email == afor.user.email end)
+        accounts = read_account()
+        accounts = List.delete(accounts, of)
+        accounts = List.delete(accounts, afor)
+
         of = %Account{of | balance: of.balance - avalue}
         afor = %Account{afor | balance: afor.balance + avalue}
-        [of, afor]
+        accounts = accounts ++ [of, afor]
+        File.write(@accounts, :erlang.term_to_binary(accounts))
     end
   end
 
