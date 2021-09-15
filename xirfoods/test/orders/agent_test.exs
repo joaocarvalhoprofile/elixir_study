9100c32968005a6c9d595d5af967f5a4128afb0d
Joao Carvalho <joaokvalho@gmail.com>
2021-09-15 16:35:47 -0300

    Add Orders agent test

---
+++ xirfoods/test/orders/agent_test.exs
@@ -0,0 +1,43 @@
+defmodule Xirfoods.Orders.AgentTest do
+  use ExUnit.Case
+
+  alias Xirfoods.Orders.Agent, as: OrderAgent
+
+  import Xirfoods.Factory
+
+  describe "save/1" do
+    test "sves the order" do
+      order = build(:order)
+
+      OrderAgent.start_link(%{})
+
+      assert {:ok, _uuid} = OrderAgent.save(order)
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      OrderAgent.start_link(%{})
+      :ok
+    end
+
+    test "when the order is found, returns the Order" do
+      order = build(:order)
+      {:ok, uuid} = OrderAgent.save(order)
+
+      response = OrderAgent.get(uuid)
+
+      expected_response = {:ok, order}
+
+      assert response == expected_response
+    end
+
+    test "when the order is not found, returns an Order not found" do
+      response = OrderAgent.get("00000000000")
+
+      expected_response = {:error, "Order not found"}
+
+      assert response == expected_response
+    end
+  end
+end
