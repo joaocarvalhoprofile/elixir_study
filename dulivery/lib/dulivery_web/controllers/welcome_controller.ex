1f92c2163eb93832e96459dfffc46a235572b3e5
Joao Carvalho <joaokvalho@gmail.com>
2021-09-16 12:56:20 -0300

    Add first route and controller

---
+++ dulivery/lib/dulivery_web/controllers/welcome_controller.ex
@@ -0,0 +1,9 @@
+defmodule DuliveryWeb.WelcomeController do
+  use DuliveryWeb, :controller
+
+  def index(conn, params) do
+    conn
+    |> put_status(:ok)
+    |> json(%{server: "dulivery web 2021"})
+  end
+end
--- dulivery/lib/dulivery_web/router.ex
+++ dulivery/lib/dulivery_web/router.ex
@@ -7,6 +7,8 @@ defmodule DuliveryWeb.Router do
 
   scope "/api", DuliveryWeb do
     pipe_through :api
+
+    get "/", WelcomeController, :index
   end
 
   # Enables LiveDashboard only for development
