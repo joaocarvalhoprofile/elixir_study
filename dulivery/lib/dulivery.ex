4fe526a82916ea278ace60c64b4b1dadabaa38b1
Joao Carvalho <joaokvalho@gmail.com>
2021-09-21 11:56:45 -0300

    Add facade items create

--- dulivery/lib/dulivery.ex
+++ dulivery/lib/dulivery.ex
@@ -4,9 +4,14 @@ defmodule Dulivery do
   alias Dulivery.Users.Get, as: UserGet
   alias Dulivery.Users.Update, as: UserUpdate
 
+  alias Dulivery.Items.Create, as: ItemCreate
+
   # Users
   defdelegate get_user_by_id(uuid), to: UserGet, as: :by_id
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate delete_user(uuid), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  # Items
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
--- dulivery/lib/dulivery_web/controllers/items_controller.ex
+++ dulivery/lib/dulivery_web/controllers/items_controller.ex
@@ -1,14 +1,13 @@
 defmodule DuliveryWeb.ItemsController do
   use DuliveryWeb, :controller
 
-  alias Dulivery.Items.Create, as: CreateUser
   alias Dulivery.Items.Item
   alias DuliveryWeb.FallbackController
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %Item{} = item} <- CreateUser.call(params) do
+    with {:ok, %Item{} = item} <- Dulivery.create_item(params) do
       conn
       |> put_status(:created)
       |> render("create.json", item: item)
